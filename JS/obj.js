bb17efcffb81d4d752b19a5ee2a2cad602582824
F4Tornado <h.rovnyak@gmail.com>
2018-05-23 14:23:00 -0400

    Thingoos

--- JS/obj.js
+++ JS/obj.js
@@ -6,20 +6,36 @@ const ammo = {
 }
 
 const Player = function() {
-  this.x = 0;
-  this.y = 0;
+  this.x = 230;
+  this.y = 128;
   this.r = 1/16;
   this.getPos = () => {
     return getPos(this.x, this.y);
   }
   this.show = () => {
-    pos = this.getPos()
+    let pos = this.getPos()
     fill(235);
     noStroke();
     ellipse(pos.x, pos.y, this.r*width, this.r*width);
   }
+  this.shoot = () => {
+
+  }
+}
+
+const bullet = (origin) => {
+  this.r = atan2(mouseY - height / 2, mouseX - width / 2);
+  this.pos = origin;
+  this.speed = 1;
+  this.size = 1/64;
+  this.show = function() {
+    let pos = getPos(pos.x, pos.y);
+    this.pos.x+=sin(this.speed);
+    this.pos.y+=cos(this.speed);
+    ellipse(pos.x, pos.y, this.size*width, this.size*width);
+  }
 }
 
 function getPos(x, y) {
-  return createVector(x/256*gameRatio.x, y/256*gameRatio.y+((height-gameRatio.y)/2));
+  return createVector(x/256*width, y/256*height);
 }
--- JS/sketch.js
+++ JS/sketch.js
@@ -1,18 +1,38 @@
 let player;
-let gameRatio;
+let cnv;
+const bullets = [];
 
 function setup() {
-  createCanvas(windowWidth, windowHeight).position(0, 0);
   player = new Player();
-  gameRatio = createVector(width, width/3*2);
+  if (windowWidth/windowHeight >= 1.5) {
+    cnv = createCanvas(windowHeight*1.5, windowHeight);
+  } else {
+    cnv = createCanvas(windowWidth, windowWidth/1.5);
+  }
 }
 
 function draw() {
   background(51);
   player.show();
+  stroke(235);
+  strokeWeight(3);
+  line(0, 0, 0, height);
+  line(0, 0, width, 0);
+  line(0, height-2, width-2, height-2);
+  line(width-2, 0, width-2, height-2);
+  for (let bullet of bullets) {
+    bullet.show();
+  }
+}
+
+function mousePressed() {
+  bullets.push(new Bullet(player.x, player.y));
 }
 
 function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
-  gameRatio = createVector(width, width/3*2);
+  if (windowWidth/windowHeight >= 1.5) {
+    resizeCanvas(windowHeight*1.5, windowHeight);
+  } else {
+    resizeCanvas(windowWidth, windowWidth/1.5);
+  }
 }
